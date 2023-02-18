d3efeaa4766dcf7e09701a5de04f1c7ab3f265b5
Naomi Tesla <naomitesla@protonmail.com>
2023-02-17 19:13:56 -0500

    Added Cut Sentence c:

---
+++ Advanced/Initiation/cut_sentence.py
@@ -0,0 +1,29 @@
+# Naomi Tesla
+# https://py.checkio.org/en/mission/cut-sentence/
+# Will be cleaned up and me more efficient later c:
+
+def cut_sentence(line: str, length: int) -> str:
+    if length < len(line):
+        truncated = line[:length].strip()
+        if truncated + ' ' not in line:
+            if truncated.rfind(' ') > -1:
+                truncated = truncated[:truncated.rfind(' ')]
+            else:
+                truncated = ''
+        return truncated + "..."
+    return line
+
+
+print("Example:")
+print(cut_sentence("Hi my name is Alex", 1))
+print(cut_sentence("Hello my name is Alex", 2))
+
+# These "asserts" are used for self-checking
+assert cut_sentence('Hi my name is Alex', 1) == '...'
+assert cut_sentence('Hello my name is Alex', 2) == '...'
+assert cut_sentence("Hi my name is Alex", 8) == "Hi my..."
+assert cut_sentence("Hi my name is Alex", 4) == "Hi..."
+assert cut_sentence("Hi my name is Alex", 20) == "Hi my name is Alex"
+assert cut_sentence("Hi my name is Alex", 18) == "Hi my name is Alex"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
