a3907f3f5ba59689e3931c68073270fdb9d61998
Naomi Tesla <naomitesla@protonmail.com>
2023-02-17 21:47:14 -0500

    Removed comments and prints c:

--- Advanced/Home/cut_sentence.py
+++ Advanced/Home/cut_sentence.py
@@ -13,15 +13,8 @@ def cut_sentence(line: str, length: int) -> str:
         return truncated + "..."
     return line
 
-
-print("Example:")
-print(cut_sentence("Hi my name is Alex", 1))
-
-# These "asserts" are used for self-checking
 assert cut_sentence('Hi my name is Alex', 1) == '...'
 assert cut_sentence("Hi my name is Alex", 8) == "Hi my..."
 assert cut_sentence("Hi my name is Alex", 4) == "Hi..."
 assert cut_sentence("Hi my name is Alex", 20) == "Hi my name is Alex"
 assert cut_sentence("Hi my name is Alex", 18) == "Hi my name is Alex"
-
-print("The mission is done! Click 'Check Solution' to earn rewards!")
