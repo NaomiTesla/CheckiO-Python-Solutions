49c9a3f8a2a6c571c5bd44b42bc9f66955ca3970
Naomi Tesla <naomitesla@protonmail.com>
2023-02-17 19:16:53 -0500

    Changed asssertions back to default!

--- Advanced/Home/cut_sentence.py
+++ Advanced/Home/cut_sentence.py
@@ -16,11 +16,9 @@ def cut_sentence(line: str, length: int) -> str:
 
 print("Example:")
 print(cut_sentence("Hi my name is Alex", 1))
-print(cut_sentence("Hello my name is Alex", 2))
 
 # These "asserts" are used for self-checking
 assert cut_sentence('Hi my name is Alex', 1) == '...'
-assert cut_sentence('Hello my name is Alex', 2) == '...'
 assert cut_sentence("Hi my name is Alex", 8) == "Hi my..."
 assert cut_sentence("Hi my name is Alex", 4) == "Hi..."
 assert cut_sentence("Hi my name is Alex", 20) == "Hi my name is Alex"
