b709eb9f80b380b9a40379d92b0eb194e46dbb21
Naomi Tesla <naomitesla@protonmail.com>
2023-02-20 17:42:13 -0500

    Added one-liner for 'Sort by Extension' c:

---
+++ Advanced/Electronic Station/sort_by_extension.py
@@ -0,0 +1,40 @@
+# Naomi Tesla
+# https://py.checkio.org/en/mission/sort-by-extension/
+
+
+def sort_by_ext(files: list[str]) -> list[str]:
+    return sorted(sorted(files), key=lambda x: (x[x.rfind('.'):]) if not x.rfind('.') else "0." + x[x.rfind('.'):])
+
+
+assert sort_by_ext(["1.cad", "1.bat", "1.aa"]) == ["1.aa", "1.bat", "1.cad"]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", "2.bat"]) == [
+    "1.aa",
+    "1.bat",
+    "2.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", ".bat"]) == [
+    ".bat",
+    "1.aa",
+    "1.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.bat", ".aa", ".bat"]) == [
+    ".aa",
+    ".bat",
+    "1.bat",
+    "1.cad",
+]
+assert sort_by_ext(["1.cad", "1.", "1.aa"]) == ["1.", "1.aa", "1.cad"]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", "1.aa.doc"]) == [
+    "1.aa",
+    "1.bat",
+    "1.cad",
+    "1.aa.doc",
+]
+assert sort_by_ext(["1.cad", "1.bat", "1.aa", ".aa.doc"]) == [
+    "1.aa",
+    "1.bat",
+    "1.cad",
+    ".aa.doc",
+]
